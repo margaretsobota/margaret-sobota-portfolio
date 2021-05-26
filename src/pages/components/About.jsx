3d2b8b5d03af4116ca9dc1b697d4800ed5455661
Margot <margaretsobota@gmail.com>
2021-05-26 17:27:05 -0500

    about me section

--- src/pages/components/About.jsx
+++ src/pages/components/About.jsx
@@ -1,19 +1,39 @@
 import React from "react";
 import "@fontsource/pridi";
 import "@fontsource/prompt";
+import Fade from 'react-reveal/Fade';
 
 const titleStyles = {
   fontFamily: "Pridi",
   fontSize: "72px",
-  color: "#3E2F5B",
-  marginTop: "20%",
+  color: "#E5E5E5",
+  marginTop: "20%"
 }
 
 const About = () => {
   return (
-    <section id="about" style={{marginTop: "300px", marginLeft: "15%"}}>
-      <h1 style={titleStyles}> A Little About Me... </h1>
-    </section>
+    <Fade left>
+      <section id="about" style={{marginTop: "300px"}}>
+        <div id="about-container" style={{marginLeft: "15%"}}>
+          <h1
+            id="about-title"
+            style={titleStyles}
+          >
+            A Little About Me...
+          </h1>
+          <p id="about-body" style={{fontFamily: "Prompt"}}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+            sed do eiusmod tempor incididunt ut labore et dolore magna
+            aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+            ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            Duis aute irure dolor in reprehenderit in voluptate velit
+            esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+            occaecat cupidatat non proident, sunt in culpa qui officia
+            deserunt mollit anim id est laborum.
+          </p>
+        </div>
+      </section>
+    </Fade>
   );
 };
 
--- src/pages/components/Headline.jsx
+++ src/pages/components/Headline.jsx
@@ -12,7 +12,6 @@ const headlineStyles = {
 const buttonStyles = {
   fontFamily: "Prompt",
   fontSize: "36px",
-  color: "#3E2F5B",
   border: "5px solid #3E2F5B",
   padding: "10px",
   filter: "drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))"
@@ -24,7 +23,15 @@ const Headline = () => {
       <h1 style={headlineStyles}>
         Hi, I'm Margaret. I like web development.
       </h1>
-      <Link to="about" style={buttonStyles}> About Me </Link>
+      <Link
+        id="about-link"
+        to="about"
+        style={buttonStyles}
+        smooth 
+        duration={1000}
+      >
+        About Me
+      </Link>
     </section>
   );
 };
