07143ef685dfbfb0f3180637f817343e1de3e4fd
Margot <margaretsobota@gmail.com>
2021-06-08 17:16:56 -0500

    blog section

--- src/pages/OXO.js
+++ src/pages/OXO.js
@@ -5,39 +5,55 @@ import { StaticImage } from "gatsby-plugin-image";
 
 const OXO = () => {
   return (
-    <section id="oxo">
-      <div className="purple-container" style={{marginLeft: "8%"}}>
-        <div className="purple-body-container" style={{marginRight: "5%"}}>
-          <h2
-            id="oxo-title"
-            className="title"
-            style={{fontFamily: "Pridi", color: "#E5E5E5"}}
-          >
-            My Internship at OXO
-          </h2>
-          <p
-            id="oxo-body"
-            style={{fontFamily: "Prompt", background: "none"}}
-          >
-            In the summer of 2020, I worked as a full-stack Software Engineer
-            Intern at a startup called
-            <a id="oxo-link" href="https://joinoxo.com/"> OXO</a>,
-            in San Francisco. OXO is a tech company that matches commuter
-            vehicles to gig drivers.
+    <div>
+      <section id="oxo">
+        <div className="purple-container" style={{marginLeft: "8%"}}>
+          <div className="purple-body-container" style={{marginRight: "5%"}}>
+            <h2
+              id="oxo-title"
+              className="title"
+              style={{fontFamily: "Pridi", color: "#E5E5E5"}}
+            >
+              My Internship at OXO
+            </h2>
+            <p
+              id="oxo-body"
+              style={{fontFamily: "Prompt", background: "none"}}
+            >
+              In the summer of 2020, I worked as a full-stack Software Engineer
+              Intern at a startup called
+              <a id="oxo-link" href="https://joinoxo.com/"> OXO</a>,
+              in San Francisco. OXO is a tech company that matches commuter
+              vehicles to gig drivers. This was an amazing experience that gave me
+              experience with fast-based, full-stack mobile development.
+            </p>
+          </div>
+          <div id="oxo-image-container">
+            <StaticImage
+              id="oxo-image"
+              src="../images/oxo2.JPG"
+              alt="Margaret at OXO"
+              layout="fixed"
+              width={400}
+              height={300}
+            />
+          </div>
+        </div>
+      </section>
+      <section id="oxo-accomplishments" style={{marginLeft: "8%"}}>
+        <div id="oxo-accomplishments-body">
+          <h3 className="subtitle" style={{fontFamily:"Pridi"}}>
+            Main Accomplishments
+          </h3>
+          <p className="description" style={{fontFamily: "Prompt"}}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+            do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+            do eiusmod tempor incididunt ut labore et dolore magna aliqua.
           </p>
         </div>
-        <div id="oxo-image-container">
-          <StaticImage
-            id="oxo-image"
-            src="../images/oxo2.JPG"
-            alt="Margaret at OXO"
-            layout="fixed"
-            width={400}
-            height={300}
-          />
-        </div>
-      </div>
-    </section>
+      </section>
+    </div>
   )
 }
 
